2e74d13f2715971a2c5738e380c7d80163c6e48d
Claudia Vieira <clau_f.v@hotmail.com>
2019-12-01 16:44:49 +0000

    conditional rendering to prevent too many api calls

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -1,16 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import "./CurrentWeather.css";
 export default function CurrentWeather () {
+
+ let [ready, setReady] = useState(null);
+let [temperature, setTemperature] = useState(null);
+ let [wind, setWind] = useState(null);
+ let [description, setDescription] = useState(null);
+ let [icon, setIcon] = useState(null);
+
+ function showCurrentWeather (response) {
+  let imageURL = `https://openweathermap.org/img/wn/${
+        response.data.weather[0].icon
+      }@2x.png`;
+  setIcon(<img src={imageURL} alt="weather icon" />);
+  setTemperature (Math.round(response.data.main.temp));
+  setWind(Math.round(response.data.wind.speed * 3.6));
+  setDescription(response.data.weather[0].description);
+  setReady(true);
+};
+
+  
+  if (ready){
     return (  <div>
-        <div className="col-xs-6">
-          <img src="" alt="" id="weatherIcon" />
+        <div className="col-xs-6" id="weatherIcon">
+          {icon}
         </div>
         <div className="col-xs-6">
           <div className="row">
             <div className="col-xs-12">
               <h1>
-                <span id="currentTemperature" />
+                <span id="currentTemperature">
+                {temperature}
+                  </span>
                 <span className="weatherUnit">
                   <a href="#" id="celsius">
                     ºC
@@ -25,15 +48,18 @@ export default function CurrentWeather () {
           </div>
           <div className="row">
             <div className="col-12">
-              <h2 id="currentWeather" />
+              <h2 id="currentWeather"> {description}</h2>
             </div>
           </div>
           <div className="row">
             <div className="col-xs-4">
             <FontAwesomeIcon icon="wind" className="currentWind" />
-              <span id="wind"> </span>
+              <span id="wind"> {wind} km/h</span>
             </div>
           </div>
         </div>
       </div>);
-};
+} else {
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=Porto&appid=011674ac65e3e0ef6d73be0d4fdbae64&units=metric`;
+  axios.get(url).then(showCurrentWeather);
+  return "Loading..."}}
