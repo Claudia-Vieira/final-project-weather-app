6c4181809ffa02ba60dfbc19438c3084f312a619
Claudia Vieira <clau_f.v@hotmail.com>
2019-12-01 15:31:17 +0000

    Display current temperature in a city

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import "./CurrentWeather.css";
 export default function CurrentWeather () {
+ let [temperature, setTemperature] = useState(null);
+function showCurrentWeather (response) {
+  setTemperature (Math.round(response.data.main.temp));
+};
+
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=Porto&appid=011674ac65e3e0ef6d73be0d4fdbae64&units=metric`;
+  axios.get(url).then(showCurrentWeather);
+
     return (  <div>
         <div className="col-xs-6">
           <img src="" alt="" id="weatherIcon" />
@@ -12,6 +21,7 @@ export default function CurrentWeather () {
               <h1>
                 <span id="currentTemperature" />
                 <span className="weatherUnit">
+                  {temperature}
                   <a href="#" id="celsius">
                     ºC
                   </a>
