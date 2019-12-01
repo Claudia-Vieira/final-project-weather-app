dc87e39a4f81cd5a7edc77bb769416f301648ace
Claudia Vieira <clau_f.v@hotmail.com>
2019-12-01 16:03:50 +0000

    Revert "Display current temperature in a city"
    This reverts commit 6c4181809ffa02ba60dfbc19438c3084f312a619.

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -1,16 +1,7 @@
-import React, {useState} from "react";
-import axios from "axios";
+import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import "./CurrentWeather.css";
 export default function CurrentWeather () {
- let [temperature, setTemperature] = useState(null);
-function showCurrentWeather (response) {
-  setTemperature (Math.round(response.data.main.temp));
-};
-
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=Porto&appid=011674ac65e3e0ef6d73be0d4fdbae64&units=metric`;
-  axios.get(url).then(showCurrentWeather);
-
     return (  <div>
         <div className="col-xs-6">
           <img src="" alt="" id="weatherIcon" />
@@ -21,7 +12,6 @@ function showCurrentWeather (response) {
               <h1>
                 <span id="currentTemperature" />
                 <span className="weatherUnit">
-                  {temperature}
                   <a href="#" id="celsius">
                     ºC
                   </a>
--- src/Forecast.js
+++ src/Forecast.js
@@ -1,40 +1,40 @@
 import React from "react";
 import "./Forecast.css";
 import Day from "./Day";
-export default function SearchForm() {
-  return (
-    <div>
-      <h4 className="forecast_title">
-        The weather at this hour in the next 5 days!
-      </h4>
-      <div className="row fiveDays">
-        <div className="col-1">
-          <p>Day</p>
+export default function SearchFOrm () {
+    return (     <div>
+        <h4 className="forecast_title">
+          The weather at this hour in the next 5 days!
+        </h4>
+        <div className="row fiveDays">
+          <div className="col-1">
+            <p>Day</p>
+          </div>
+          <div className="col-1">
+            <p />
+          </div>
+          <div className="col-3">
+            <p>Temperature</p>
+          </div>
+          <div className="col-4">
+            <p>Weather</p>
+          </div>
+          <div className="col-2">
+            <p>Wind</p>
+          </div>
         </div>
-        <div className="col-1">
-          <p />
+        <div id="forecast">
+            <Day value={1} />
+          
+            <Day value={2} />
+        
+          
+            <Day value={3} />
+         
+            <Day value={4} />
+        
+            <Day value={5} />
+   
         </div>
-        <div className="col-3">
-          <p>Temperature</p>
-        </div>
-        <div className="col-4">
-          <p>Weather</p>
-        </div>
-        <div className="col-2">
-          <p>Wind</p>
-        </div>
-      </div>
-      <div id="forecast">
-        <Day value={1} />
-
-        <Day value={2} />
-
-        <Day value={3} />
-
-        <Day value={4} />
-
-        <Day value={5} />
-      </div>
-    </div>
-  );
-}
+      </div>)
+};
