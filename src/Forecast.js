0b2a96ac23b11721a0e2fb3092073b93578aa997
Claudia Vieira <clau_f.v@hotmail.com>
2019-12-03 02:42:44 +0000

    added forecast

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,87 +1,55 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import WeatherIcon from "./WeatherIcon.js"
 import "./Forecast.css";
-import Day from "./Day";
-export default function Forecast (props) {
 
+export default function Forecast(props) {
+  const [loaded, setLoaded] = useState(false);
+  const [forecast, setForecast] = useState(null);
+  function handleResponse(response) {
+    setForecast(response.data);
+    setLoaded(true);
+  }
 
-  let [forecastData, setForecastData] = useState (null);
+  function formatHours(date) {
+    let hours = date.getHours();
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    return `${hours}:${minutes}`;
+  }
 
-if (props.chosenCity) {
-  function displayForecast(response) {
-    setForecastData(response.data);
+  if (loaded && forecast.city.name === props.chosenCity) {
+    return (<div className="row">
+      <div className="col-12">
+            <h4 className="forecast_title">
+              The weather at this hour in the next 24hours!
+            </h4>
+            </div>
+            <div className="WeatherForecast row">
+        {forecast.list.slice(0,7).map(function(weather) {
+          return (
+            
+            
+            <div className="col">
+              {console.log (forecast.list)}
+              {formatHours(new Date(weather.dt * 1000))}
+              <WeatherIcon code={weather.weather[0].icon} />
+              {Math.round(weather.main.temp)}°C
+            </div>
+
+         );
+        })}
+      </div>
+      </div>);
+  } else {
+
+    let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${props.chosenCity}&appid=011674ac65e3e0ef6d73be0d4fdbae64&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return null;
+  }
 }
-
-  let forecastUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${props.chosenCity}&mode=xml&appid=011674ac65e3e0ef6d73be0d4fdbae64&units=metric`;
-  axios.get(forecastUrl).then(displayForecast);
-
-console.log(forecastData);
-    return (
-    <div>
-        <h4 className="forecast_title">
-          The weather at this hour in the next 5 days!
-        </h4>
-        <div className="row fiveDays">
-          <div className="col-1">
-            <p>Day</p>
-          </div>
-          <div className="col-1">
-            <p />
-          </div>
-          <div className="col-3">
-            <p>Temperature</p>
-          </div>
-          <div className="col-4">
-            <p>Weather</p>
-          </div>
-          <div className="col-2">
-            <p>Wind</p>
-          </div>
-        </div>
-        <div id="forecast">
-            <Day value={1} />
-          
-            <Day value={2} />
-        
-            <Day value={3} />
-         
-            <Day value={4} />
-        
-            <Day value={5} />
-   
-        </div>
-      </div>)
-} else {return (<div>
-  <h4 className="forecast_title">
-    The weather at this hour in the next 5 days!
-  </h4>
-  <div className="row fiveDays">
-    <div className="col-1">
-      <p>Day</p>
-    </div>
-    <div className="col-1">
-      <p />
-    </div>
-    <div className="col-3">
-      <p>Temperature</p>
-    </div>
-    <div className="col-4">
-      <p>Weather</p>
-    </div>
-    <div className="col-2">
-      <p>Wind</p>
-    </div>
-  </div>
-  <div id="forecast">
-      <Day value={1} />
-    
-      <Day value={2} />
-  
-      <Day value={3} />
-   
-      <Day value={4} />
-  
-      <Day value={5} />
-
-  </div>
-</div>)}}
